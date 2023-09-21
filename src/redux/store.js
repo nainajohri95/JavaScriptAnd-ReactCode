1a5bd82c86e47155c3cd8ffb60a41f795d919677
Naina Johri <93525285+nainajohri95@users.noreply.github.com>
2023-09-21 23:50:12 +0530

    redux

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import messageReducer from "./slices/messageSlice";
-
-export default configureStore({
-  reducer: {
-    messageReducer, //here key and value are same as messageReducre
-  },
-});
